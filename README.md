ba5c242f2a6899c4665c19df4b5205dfa6e0cafe
Petr <petr@duck.com>
2024-01-21 14:46:49 +0300

    first minimal working version

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tmux session management for Fish shell
+
+Provides two functions:
+
+1. **tm** to create sessions or conbect to existing ones
+2. **tml** to list sessions
+
+Usage: TODO
---
+++ functions/tm.fish
@@ -0,0 +1,15 @@
+function tm -d "Connect to Tmux session or create one"
+    if count $argv >/dev/null
+        tmux a -t $argv || tmux new -s $argv
+    else
+        tmux a || tmux
+    end
+end
+function tml -d "List Tmux sessions"
+    tmux list-sessions
+end
+# if not set -q TMUX
+#     set -g TMUX tmux new-session -d -s base
+#     eval $TMUX
+#     tmux attach-session -d -t base
+# end
